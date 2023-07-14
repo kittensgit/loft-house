3c5dbd86427d6af87b8c98260337a7c4c999c812
Nika <nikaonime@gmail.com>
2023-07-14 13:17:51 +0300

    JS mask phone

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // Nav icon logic
 
-const navBtn  = document.querySelector('.nav-icon-btn');
+const navBtn = document.querySelector('.nav-icon-btn');
 const navIcon = document.querySelector('.nav-icon');
 const nav = document.querySelector('.header__top-row')
 
@@ -9,3 +9,19 @@ navBtn.onclick = function () {
     nav.classList.toggle('header__top-row--mobile');
     document.body.classList.toggle('no-scroll')
 }
+
+// Phone mask 
+
+mask('[data-tel-input]');
+
+// remove '+' and show placeholder
+
+const phoneInput = document.querySelectorAll('[data-tel-input]');
+phoneInput.forEach((input) => {
+    input.addEventListener('input', () => {
+        if (input.value === '+') input.value = '';
+    })
+    input.addEventListener('blur', () => {
+        if (input.value === '+') input.value = '';
+    })
+});
